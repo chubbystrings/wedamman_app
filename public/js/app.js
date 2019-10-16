745c7591769dbc3d94f6d2387c0b3e282ae82f9c
chubbystrings <martinsokwor@hotmail.com>
2019-10-16 07:34:07 +0100

    addded digital clock and some modifications

--- public/js/app.js
+++ public/js/app.js
@@ -13,15 +13,62 @@ const searchDiv = document.getElementById('searchDiv');
 const searchLocation = document.getElementById('searchLocation');
 const btnSearch = document.getElementById('btnSearch');
 const inputError = document.getElementById('inputError');
+const clock = document.getElementById('clock');
+const formatedDate = document.getElementById('formatedDate');
 
-// const IdsRollerTwo = document.getElementById('Ids-rollerTwo');
-// const container = document.getElementById('container');
+//months of the year object
+const MonthOfTheYear = {
+    01: "Jan",
+    02: "Feb",
+    03: "Mar",
+    04: "Apr",
+    05: "May",
+    06: "Jun",
+    07: "Jul",
+    08: "Aug",
+    09: "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec"
 
-//variable initialization
+}
+
+//function for digital clock ticking with setIterval
+const ticking = () => { 
+    let newTime = new Date().toLocaleTimeString()
+  
+    return newTime
+}
+
+//function to get easy readable date 
+const convertDate = () => {
+    let dayOfTheWeek = new Date().toString()
+    dayOfTheWeek = dayOfTheWeek.split(' ')[0]
+    let newDate = new Date().toLocaleDateString();
+    let month = newDate.split('/')[0]
+    let day = newDate.split('/')[1]
+    let year = newDate.split('/')[2]
+
+    return `${dayOfTheWeek} ${day} ${MonthOfTheYear[month]}, ${year}`
+
+}
+
+//render date and  time on the dom
+clock.textContent = ticking()
+formatedDate.textContent = convertDate()
+
+//make digital clock tick every Second
+setInterval(()=> {
+  clock.textContent =  ticking()
+}, 1000)
+
+//  variable initialization for received data from http request
 let coords;
 let latitude;
 let longitude;
 let date;
+
+
 
 
 //Event Listener for forecast button
@@ -56,7 +103,8 @@ autoWeather.addEventListener('click', e => {
     })
 })
 
-
+const char = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+//event listener for search button
 btnSearch.addEventListener('click', (e) =>{
     
     e.preventDefault()
@@ -65,6 +113,7 @@ btnSearch.addEventListener('click', (e) =>{
         inputError.textContent = "please enter a valid Location!!!";
         return false
     }
+    if(char.test(location) === true){ inputError.textContent = "We don't do that here, remove CHARACTERS"; return false}
 
     hideElements()
 
@@ -87,26 +136,28 @@ btnSearch.addEventListener('click', (e) =>{
 
 
 
-
+//function to hide elements when displaying info
 
 const hideElements = () => {
     inputError.textContent = "";
     searchDiv.style.display = "none"
     autoWeather.style.display = "none"
-    home.style.visibility = 'hidden'
+    home.style.display = 'none'
     IdsRoller.style.removeProperty('display')
     messageOne.innerText = "Hold On a Sec, Gathering Location Information..........."
 
 }
 
-
+//function to display forecast data on DOM element
 const displayForecast = (weather) => {
     
-    console.log(weather)
+    // console.log(weather)
     header.style.display = "none"
     intro.style.display = "none"
     IdsRoller.style.display = 'none'
     messageOne.innerText = ''
     weather.location? messageTwo.innerText =` In ${weather.location} ${weather.forecast}` : messageTwo.innerText = weather.forecast;
-    home.style.removeProperty('visibility')
+    home.style.display = "inline"
 }
+
+
